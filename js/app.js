453850e60bb6dc5f16a6bac23a7f027154fba183
Carlos Plata <Carlos.plata069@gmail.com>
2024-01-23 22:31:43 -0600

    added fake contacts functionality

--- js/app.js
+++ js/app.js
@@ -1,5 +1,6 @@
 const body = document.body;
-const formulary = body.getElementsByClassName("form-container")
+const formulary = body.getElementsByClassName("form-container");
+const contacts = body.getElementsByClassName("contact-search");
 
 const loadingText = document.createElement("span");
 loadingText.textContent = "Cargando...";
@@ -28,6 +29,44 @@ async function submitForm(form) {
   toggleForm();
   
   console.log(data);
+}
+
+async function searchContacts() {
+  contacts[0].append(loadingText);
+  responseText.remove();
+
+  const response = await fetch("https://reqres.in/api/users?delay=4");
+  const data = await response.json();
+
+  loadingText.remove();
+  
+  data.data.forEach(element => {
+    const {first_name} = element;
+    const {last_name} = element;
+    const {email} = element;
+    const {avatar} = element;
+
+    const container = developElement("div", "contact-card");
+    const containerDetails = developElement("div");
+    const image = developElement("img", "contact-card-image", null, "src", avatar);
+    const personName = developElement("p", "contact-card-name", `${first_name} ${last_name}`);
+    const personEmail = developElement("p", "contact-card-email", email)
+
+    container.append(image, containerDetails);
+    containerDetails.append(personName, personEmail);
+
+    contacts[0].append(container);
+  });
+}
+
+function developElement(tag, classname, text, attributename, attributevalue) {
+  const newElement = document.createElement(tag);
+
+  if (classname != null) newElement.classList.add(classname);
+  if (text != null) newElement.textContent = text;
+  if (attributename != null && attributevalue != null) newElement.setAttribute(attributename, attributevalue);
+
+  return newElement;
 }
 
 function toggleForm() {
